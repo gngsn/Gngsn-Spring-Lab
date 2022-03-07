ea5b68ccfc17e84caf8a50e6b4c00319c0f2bca8
gngsn <rudtjs4540@gmail.com>
2022-03-07 21:31:07 +0900

    toby: Exception

--- spring-toby/toby/src/main/java/com/gngsn/toby/TobyApplication.java
+++ spring-toby/toby/src/main/java/com/gngsn/toby/TobyApplication.java
@@ -1,9 +1,14 @@
 package com.gngsn.toby;
 
+import ch.qos.logback.core.net.SocketConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.sql.ResultSet;
 
 @SpringBootApplication
 @PropertySource(value = {
