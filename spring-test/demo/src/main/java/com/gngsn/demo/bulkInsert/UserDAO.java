4342f419690dd7c00bf08960a7a6c1cdbf0eeded
gngsn <rudtjs4540@gmail.com>
2022-03-29 23:51:07 +0900

    test: Bulk Insert

--- spring-test/demo/src/main/java/com/gngsn/demo/bulkInsert/UserDAO.java
+++ spring-test/demo/src/main/java/com/gngsn/demo/bulkInsert/UserDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public class UserDAO {
-    private static final String NAMESPACE = "com.gngsn.demo.bulkInsert.UserDAO.";
+    private static final String NAMESPACE = "bulkInsert.UserDAO.";
 
     @Autowired
     private SqlSession sqlSession;
--- spring-test/demo/src/main/java/com/gngsn/demo/bulkInsert/UserInsertRunner.java
+++ spring-test/demo/src/main/java/com/gngsn/demo/bulkInsert/UserInsertRunner.java
@@ -10,36 +10,38 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Log4j2
-//@Component
-//public class UserInsertRunner implements ApplicationRunner {
-//    @Autowired
-//    private InsertService insertService;
-//
-//    private List<User> users = new ArrayList<>();
-//
-//    @Override
-//    public void run(ApplicationArguments args) throws Exception {
-//        log.info("\n\n========== Runner ===========\n\n");
-//        genUser();
-//        log.info("Test Size : " + users.size());
-//
-//        long basicDuration = insertService.basicInsert(users);
-//        long bulkDuration = insertService.bulkInsert(users);
-//
-//        log.info("Basic Insert Duration : " + basicDuration);
-//        log.info("Bulk Insert Duration : " + bulkDuration);
-//
-//        log.info("\n\n=============================\n\n");
-//    }
-//
-//    private void genUser() {
-//        for (int i=0; i < 10000; i++) {
-//            users.add(new User(
-//                    "gngsn" + i,
-//                    "gngsn" + i + "@email.com",
-//                    new RandomString().nextString())
-//            );
-//        }
-//    }
-//
+@Log4j2
+@Component
+public class UserInsertRunner implements ApplicationRunner {
+    @Autowired
+    private InsertService insertService;
+
+    private final int INSERT_SIZE = 10_000;
+
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("\n========== Runner ===========\n");
+        log.info("Test Size : " + INSERT_SIZE);
+        genUser();
+
+        long basicDuration = insertService.basicInsert(users);
+        long bulkDuration = insertService.bulkInsert(users);
+
+        log.info("Basic Insert Duration : " + basicDuration);
+        log.info("Bulk Insert Duration : " + bulkDuration);
+
+        log.info("\n=============================\n");
+    }
+
+    private void genUser() {
+        for (int i = 0; i < INSERT_SIZE; i++) {
+            users.add(new User(
+                    "gngsn" + i,
+                    "gngsn" + i + "@email.com",
+                    new RandomString().nextString())
+            );
+        }
+    }
+}
