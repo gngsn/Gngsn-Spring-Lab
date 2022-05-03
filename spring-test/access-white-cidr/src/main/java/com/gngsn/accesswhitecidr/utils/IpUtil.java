7641399cd747dd128ea2648a405adfe276c9f1b0
gngsn <rudtjs4540@gmail.com>
2022-05-03 23:38:29 +0900

    demo: access white cidr

--- spring-test/access-white-cidr/src/main/java/com/gngsn/accesswhitecidr/utils/IpUtil.java
+++ spring-test/access-white-cidr/src/main/java/com/gngsn/accesswhitecidr/utils/IpUtil.java
@@ -15,13 +15,12 @@ public class IpUtil {
      */
     private static final String[] IP_HEADER_CANDIDATES = {
         "X-Forwarded-For",
+        "HTTP_X_FORWARDED_FOR",
         "Proxy-Client-IP",
         "WL-Proxy-Client-IP",
-        "HTTP_X_FORWARDED_FOR",
         "HTTP_X_FORWARDED",
         "HTTP_X_CLUSTER_CLIENT_IP",
         "HTTP_CLIENT_IP",
-        "HTTP_FORWARDED_FOR",
         "HTTP_FORWARDED",
         "HTTP_VIA",
         "REMOTE_ADDR"
