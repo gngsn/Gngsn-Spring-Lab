fbafdf18ba7b2d230e81d2d75ff12dcb54f78f41
gngsn <rudtjs4540@gmail.com>
2022-05-11 23:18:53 +0900

    demo: clean code with java enum

--- spring-test/demo/src/main/java/com/gngsn/demo/cleanCodeWithEnum/TransactionType.java
+++ spring-test/demo/src/main/java/com/gngsn/demo/cleanCodeWithEnum/TransactionType.java
@@ -1,4 +1,41 @@
 package com.gngsn.demo.cleanCodeWithEnum;
 
-public class TransactionType {
+public enum TransactionType {
+
+    BUY {
+        @Override
+        public void doTransaction(Account account, long cash) {
+            long tax = Math.round(cash * 0.15 / 100);
+            cash = cash + tax;
+            account.withdraw(cash);
+        }
+    },
+    SELL {
+        @Override
+        public void doTransaction(Account account, long cash) {
+            long tax = Math.round(cash * 0.1 / 100);
+            cash = cash + tax;
+            account.deposit(cash);
+        }
+    },
+    DEPOSIT {
+        @Override
+        public void doTransaction(Account account, long cash) {
+            long tax = Math.round(cash * 0.05 / 100);
+            cash = cash + tax;
+            account.deposit(cash);
+        }
+    },
+    WITHDRAWAL {
+        @Override
+        public void doTransaction(Account account, long cash) {
+            long tax = Math.round(cash * 0.20 / 100);
+            cash = cash + tax;
+            account.withdraw(cash);
+        }
+    };
+
+    public abstract void doTransaction(Account account, long cash);
+
+
 }
