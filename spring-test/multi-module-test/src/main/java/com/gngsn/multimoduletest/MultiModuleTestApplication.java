6a75ae942f9294feb751ff6a233862886e920693
gngsn <rudtjs4540@gmail.com>
2022-06-27 23:17:07 +0900

    init: multi module test

---
+++ spring-test/multi-module-test/src/main/java/com/gngsn/multimoduletest/MultiModuleTestApplication.java
@@ -0,0 +1,13 @@
+package com.gngsn.multimoduletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleTestApplication.class, args);
+    }
+
+}
