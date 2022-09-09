f546d2c66abde96ac901cfded6f2055df8a21a7f
gngsn <rudtjs4540@gmail.com>
2022-09-10 02:02:25 +0900

    update: kotlin range (in)

--- demo/kotlin-demo/src/test/kotlin/com/gngsn/kotlindemo/Basic_for.kt
+++ demo/kotlin-demo/src/test/kotlin/com/gngsn/kotlindemo/Basic_for.kt
@@ -1,6 +1,7 @@
 package com.gngsn.kotlindemo;
 
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class Basic_for {
     // 순차적으로 수를 세면서 3으로 나눠떨어지는 수에 대해서는 Fizz, 5로 나눠떨어지는 수에 대해 서는 Buzz 라고 말해야 한다.
@@ -13,8 +14,45 @@ class Basic_for {
     }
 
     @Test
-    fun ex_iter() {
+    fun ex_iter_downTo() {
         // 100 downTo 1은 역방향 수열을 만든다 역방향 수열의 기본 증가 값은 -1
         for (i in 100 downTo 1 step 2) print(fizzBuzz(i))
+    }
+
+    private fun ex_iter(): TreeMap<Char, String> {
+        val binaryReps = TreeMap<Char, String>()
+
+        // for in .. 범위 기반 iteration
+        for (c in 'A'..'F') {
+            val binary = Integer.toBinaryString(c.code)
+            binaryReps[c] = binary // Java에서 binary.put(c, binary) 와 동일
+        }
+        return binaryReps
+    }
+
+    @Test
+    fun ex_iter_main() {
+        ex_iter()
+    }
+
+
+    @Test
+    fun ex_for_In_map() {
+        val binaryReps = ex_iter()
+
+        // for..in 구문에 map을 쓰면 (key, value) 값 반환
+       for ((letter, binary) in binaryReps) {
+            println("$letter = $binary")
+        }
+    }
+
+    @Test
+    fun ex_for_in_list() {
+        val list = arrayListOf ("10", "11", "1001")
+
+        // withIndex: 인덱스와 함께 컬렉션을 이터레이션
+        for ((index, element) in list.withIndex()) {
+            println("$index: $element")
+        }
     }
 }
---
+++ demo/kotlin-demo/src/test/kotlin/com/gngsn/kotlindemo/Basic_inKeyword.kt
@@ -0,0 +1,50 @@
+package com.gngsn.kotlindemo;
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class Basic_inKeyword {
+
+    fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+    fun isNotDigit(c: Char) = c !in '0'..'9'  // '0'<= c && x<= '9' 로 변환된다
+
+    @Test
+    fun ex_range() {
+        val oneToTen = 1..10
+        println("oneToTen : $oneToTen")      // 1..10
+        println("class: ${oneToTen.javaClass}")  // class kotlin.ranges.IntRange
+
+        val aToZ = 'a'..'z'
+        println("oneToTen : $aToZ")      // a..z
+        println("class: ${aToZ.javaClass}")  // class kotlin.ranges.CharRange
+
+        // comparable 인터페이스를 구현한 클래스로 범위 생성
+        val comparableRange = "Java".."Scala"           // "Java" <= "Kotlin" && "Kotlin” 〈= "Scala"와 동일
+        println("oneToTen : $comparableRange")          // Java..Scala
+        println("class: ${comparableRange.javaClass}")  // class kotlin.ranges.ComparableRange
+    }
+
+    @Test
+    fun ex_for_In() {
+        Assertions.assertTrue(isLetter('q'))
+        Assertions.assertTrue(isNotDigit('x'))
+    }
+
+    private fun recognize(c: Char) = when (c) {
+        in '0'..'9' -> "It's a digit!"
+        in 'a'..'z', in 'A'..'Z' -> "It's a letter!"
+        else -> "I don't know..."
+    }
+
+    @Test
+    fun ex_when_In() {
+        Assertions.assertEquals("It's a digit!", recognize('8'))
+        Assertions.assertEquals("It's a letter!", recognize('x'))
+    }
+
+    @Test
+    fun ex_set_In() {
+        Assertions.assertFalse("Kotlin" in setOf ("Java", "Scala"))
+    }
+}
