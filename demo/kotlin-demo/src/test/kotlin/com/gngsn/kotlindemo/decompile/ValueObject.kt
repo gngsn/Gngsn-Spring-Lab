a5306f383bd759d1660a3ba45631eebd9c42d5ce
gngsn <rudtjs4540@gmail.com>
2022-09-24 23:31:24 +0900

    update: decompile various classes

---
+++ demo/kotlin-demo/src/test/kotlin/com/gngsn/kotlindemo/decompile/ValueObject.kt
@@ -0,0 +1,21 @@
+package com.gngsn.kotlindemo.decompile
+
+class ValueObject1(
+    val val1: String,
+    val val2: String
+)
+
+
+class ValueObject2(
+    var var1: String,
+    var var2: String
+)
+
+data class ValueObject3(
+    val elem: String,
+)
+
+
+data class ValueObject4(
+    var elem: String,
+)
