e2c49475dc6163e01296ee60a6187bcdcad6de7d
gngsn <rudtjs4540@gmail.com>
2022-10-18 23:33:17 +0900

    update: kotlin lambda with Receiver

---
+++ demo/kotlin-demo/src/test/kotlin/com/gngsn/kotlindemo/ch5/Receivers.kt
@@ -0,0 +1,57 @@
+package com.gngsn.kotlindemo.ch5
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Receivers {
+
+    fun alphabet(): String {
+        val result = StringBuilder()
+        for (letter in 'A'..'Z') {
+            result.append(letter)
+        }
+        result.append("\nNow I know the alphabet!")
+        return result.toString()
+    }
+
+    fun alphabetRefac(): String {
+        val stringBuilder = StringBuilder()
+        return with(stringBuilder) {
+            for (letter in 'A'..'Z') {
+                append(letter)
+            }
+            append("\nNow I know the alphabet!")
+            this.toString()
+        }
+    }
+
+    fun alphabetRefac2() = with(StringBuilder()) {
+        for (letter in 'A'..'Z') {
+            append(letter)
+        }
+
+        append("\nNow I know the alphabet!")
+        toString()
+    }
+
+
+    @Test
+    fun withTest() {
+        Assertions.assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ\nNow I know the alphabet!", alphabet())
+        Assertions.assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ\nNow I know the alphabet!", alphabetRefac())
+    }
+
+
+    fun alphabetRefacApply() = StringBuilder().apply {
+        for (letter in 'A'..'Z') {
+            append(letter)
+        }
+        append("\nNow I know the alphabet!")
+    }.toString()
+
+
+    @Test
+    fun applyTest() {
+        Assertions.assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ\nNow I know the alphabet!", alphabetRefacApply())
+    }
+}
---
+++ demo/kotlin-demo/src/test/kotlin/com/gngsn/kotlindemo/ch5/SamConstructure.kt
@@ -0,0 +1,21 @@
+package com.gngsn.kotlindemo.ch5
+
+class SamConstructure {
+    fun createAllDoneRunnable(): Runnable {
+        return Runnable { println("All done! ")
+        }
+    }
+
+//    val listener = OnClickListener { view ->
+//        val text = when (view.id) { // view.id를 사용해 어떤 버튼이 클릭됐는지 판단한다.
+//            R.id.buttonl -> "First button"
+//            R.id.button2 -> "Second button"
+//            else -> "Unknown button"
+//        }
+//
+//        toast(text)  // "text"의 값을 사용자에게 보여준다.
+//    }
+//
+//    buttonl.setOnClickListener(listener)
+//    button2.setOnClickListener(listener)
+}
