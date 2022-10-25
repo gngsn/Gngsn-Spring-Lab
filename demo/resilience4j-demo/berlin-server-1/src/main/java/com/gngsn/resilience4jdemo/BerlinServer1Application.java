611cc9e7455b88aeb88244ab84add2e3c0b6882b
gngsn <rudtjs4540@gmail.com>
2022-10-25 22:11:52 +0900

    init: resilience4j demo init

---
+++ demo/resilience4j-demo/berlin-server-1/src/main/java/com/gngsn/resilience4jdemo/BerlinServer1Application.java
@@ -0,0 +1,13 @@
+package com.gngsn.resilience4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerlinServer1Application.class, args);
+    }
+
+}
