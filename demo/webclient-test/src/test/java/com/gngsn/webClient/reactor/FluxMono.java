7ff3c5ec42403b365968a48e61420c59646c3f49
gngsn <rudtjs4540@gmail.com>
2022-11-25 00:02:06 +0900

    update: reactor project mono & flux

--- demo/webclient-test/src/test/java/com/gngsn/webClient/reactor/FluxMono.java
+++ demo/webclient-test/src/test/java/com/gngsn/webClient/reactor/FluxMono.java
@@ -6,8 +6,13 @@ import org.slf4j.LoggerFactory;
 import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FluxMono {
@@ -73,4 +78,41 @@ public class FluxMono {
             .subscribe(i -> System.out.println("subscribe: " + i)) // 4 (onComplete)
         ;
     }
+
+    @Test
+    public void monoMethod() {
+        final List<String> basket1 = Arrays.asList("kiwi", "orange", "lemon", "orange", "lemon", "kiwi");
+        final List<String> basket2 = Arrays.asList("banana", "lemon", "lemon", "kiwi");
+        final List<String> basket3 = Arrays.asList("strawberry", "orange", "lemon", "grape", "strawberry");
+        final List<List<String>> baskets = Arrays.asList(basket1, basket2, basket3);
+        final Flux<List<String>> basketFlux = Flux.fromIterable(baskets);
+
+        basketFlux.concatMap(basket -> {
+
+            log.info("basket : {}", basket);
+            final Mono<List<String>> distinctFruits = Flux.fromIterable(basket).distinct().collectList().subscribeOn(Schedulers.parallel());
+
+            log.info("distinctFruits : {}", distinctFruits);
+
+            final Mono<Map<String, Long>> countFruitsMono = Flux.fromIterable(basket)
+                .groupBy(fruit -> fruit) // 바구니로 부터 넘어온 과일 기준으로 group을 묶는다.
+                .concatMap(groupedFlux -> groupedFlux.count()
+                    .map(count -> {
+                        final Map<String, Long> fruitCount = new LinkedHashMap<>();
+                        fruitCount.put(groupedFlux.key(), count);
+                        return fruitCount;
+                    }) // 각 과일별로 개수를 Map으로 리턴
+                ) // concatMap으로 순서보장
+                .reduce((accumulatedMap, currentMap) -> new LinkedHashMap<String, Long>() { {
+                    putAll(accumulatedMap);
+                    putAll(currentMap);
+                }}) // 그동안 누적된 accumulatedMap에 현재 넘어오는 currentMap을 합쳐서 새로운 Map을 만든다. // map끼리 putAll하여 하나의 Map으로 만든다.
+                .subscribeOn(Schedulers.parallel());
+
+
+            log.info("countFruitsMono : {}", countFruitsMono);
+            return Flux.zip(distinctFruits, countFruitsMono, FruitInfo::new);
+        }).subscribe(System.out::println);
+    }
 }
+
