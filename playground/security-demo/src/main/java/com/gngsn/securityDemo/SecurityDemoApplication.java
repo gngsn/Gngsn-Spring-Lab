fa8860f95eb1aeac86fec61e1fff606fa28ca7dc
gngsn <rudtjs4540@gmail.com>
2023-03-08 23:59:24 +0900

    update: config setting

---
+++ playground/security-demo/src/main/java/com/gngsn/securityDemo/SecurityDemoApplication.java
@@ -0,0 +1,10 @@
+package com.gngsn.securityDemo;
+
+@SpringBootApplication
+public class SecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
+
+}
