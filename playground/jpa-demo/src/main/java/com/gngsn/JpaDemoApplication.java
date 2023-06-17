5f1fe9334d07a413e8045ac39cb25b02df186136
gngsn <rudtjs4540@gmail.com>
2023-06-17 19:31:21 +0900

    refactor JPA demo

--- playground/jpa-demo/src/main/java/com/gngsn/JpaDemoApplication.java
+++ playground/jpa-demo/src/main/java/com/gngsn/JpaDemoApplication.java
@@ -9,18 +9,4 @@ public class JpaDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(JpaDemoApplication.class, args);
     }
-
-//    @Bean
-//    public ApplicationRunner configure(UserRepository userRepository) {
-//        return env -> {
-//            User user1 = new User("gngsn", LocalDate.of(2023, Month.AUGUST, 12));
-//            User user2 = new User("mike", LocalDate.of(2020, Month.JANUARY, 18));
-//
-//            userRepository.save(user1);
-//            userRepository.save(user2);
-//
-//            userRepository.findAll().forEach(System.out::println);
-//        };
-//    }
-
 }
