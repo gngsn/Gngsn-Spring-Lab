0972cdbeafd41d683f79d1e1eacd1d78e28aeeaa
gngsn <enfj.dev@gmail.com>
2024-02-15 00:06:22 +0900

    modify: change module name

--- playground/transaction-demo/src/main/java/com/gngsn/transactionTest/TransactionTestApplication.java
+++ playground/transaction-demo/src/main/java/com/gngsn/transactionTest/TransactionTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebclientTestApplication {
+public class TransactionTestApplication.java {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebclientTestApplication.class, args);
+        SpringApplication.run(TransactionTestApplication.java.class, args);
     }
 
 }
